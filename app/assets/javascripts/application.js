587521d7b7b0a1f06e43bf2322a41a837e991e1e
3436 <3436@example.com>
2020-12-23 10:37:39 +0900

    画像の追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,17 +5,17 @@
 //= require jquery_ujs
 
 $(function(){
-$("#password-field").click(function() {
-  // iconの切り替え
-  $(this).toggleClass("fa-eye fa-eye-slash");
+  $("#password-field").click(function() {
+    // iconの切り替え
+    $(this).toggleClass("fa-eye fa-eye-slash");
 
-  // 入力フォームの取得
-  var input = $(this).parent().prev("input");
-  // type切替
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
-  } else {
-    input.attr("type", "password");
-  }
-});
+    // 入力フォームの取得
+    var input = $(this).parent().prev("input");
+    // type切替
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
 });
