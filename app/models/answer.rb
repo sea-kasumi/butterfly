d0524a75e14b1416148bf46d00ea5cb9e9c30a26
3424 <3424@example.com>
2020-12-23 10:25:16 +0900

    質問コーナーデザイン

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,7 +6,7 @@ class Answer < ApplicationRecord
 
     def error_check
         if answer.blank?
-            errors[:base]<< '回答は必ず入力してください'
+            errors[:base]<< '回答を入力してください'
         end
     end
   
